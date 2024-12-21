65bdd60a1495e5edd19cb0c49d52425d5ffb3070
sourav <soutavr5@gmail.com>
2024-12-21 11:13:05 +0530

    sec

--- frtoentd/src/App.jsx
+++ frtoentd/src/App.jsx
@@ -1,6 +1,5 @@
-
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PageWrapper from './loading/PageWrapper';
 import Login from './components/Login';
 import Signup from './components/Signup';
@@ -10,8 +9,10 @@ function App() {
     return (
         <Router>
             <Routes>
-            <Route path="/login" element={<PageWrapper><Login /></PageWrapper>} />
-            <Route path="/signup" element={<PageWrapper><Signup /></PageWrapper>} />
+                {/* Redirect root path to login */}
+                <Route path="/" element={<Navigate to="/login" />} />
+                <Route path="/login" element={<PageWrapper><Login /></PageWrapper>} />
+                <Route path="/signup" element={<PageWrapper><Signup /></PageWrapper>} />
                 <Route path="/dashboard" element={<Dashboard />} />
             </Routes>
         </Router>
